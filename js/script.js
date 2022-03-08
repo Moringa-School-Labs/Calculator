6663418f4f0c20727e0cf22e576d02a1ef1ac3e2
esthercate <vuthi.esther@gmail.com>
2022-03-08 12:47:05 +0300

    add user interface

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
-var add  = function(number1, number2) {
+// Business Logic or Back-end
+var add = function (number1, number2) {
   return number1 + number2;
-  };
+};
 
-  var subtract = function(number1, number2) {
-    return number1 - number2;t
-  };
+var subtract = function (number1, number2) {
+  return number1 - number2;
+};
 
-  var multiply = function(number1, number2) {
-    return number1 * number2;
-  };
+var multiply = function (number1, number2) {
+  return number1 * number2;
+};
 
-  var divide = function(number1, number2) {
-    return number1 / number2;
-  };
+var divide = function (number1, number2) {
+  return number1 / number2;
+};
 
-  var number1 = parseInt(prompt("Enter a number:"));
-  var number2 = parseInt(prompt("Enter another number:"));
-  var result = add(number1, number2);
-  alert(result);
+// User interface logic or front-end
+$(document).ready(function () {
+  $("form#add").submit(function (event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
